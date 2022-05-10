77cb8374cfb86bfb747c1d263cad02e2a45bee82
AL <26797547+Al12rs@users.noreply.github.com>
2022-05-10 16:45:32 +0200

    Initial commit with template code for aircraft position
    Co-authored-by: GrikFreak <GrikFreak@users.noreply.github.com>

---
+++ backend/aircraft_positions/aircraft_positions.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"sync"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/gin-gonic/gin"
+)
+
+const env_credJson = "GOOGLE_APPLICATION_CREDENTIALS"
+
+const env_projectID = "GOOGLE_CLOUD_PROJECT_ID"
+const env_topicID = "GOOGLE_PUBSUB_AIRCRAFT_LIST_TOPIC_ID"
+const env_subID = "GOOGLE_PUBSUB_AIRCRAFT_LIST_SUBSCRIBER_ID"
+
+const env_port = "PORT"
+const env_ginmode = "GIN_MODE"
+
+type AircraftList struct {
+	sync.RWMutex
+	val string
+}
+
+type PubSubListener struct {
+	icao24    string
+	wsClients map[interface{}]interface{}
+}
+
+func startPubSubListener(state *PubSubListener) {
+	
+	sub, err := .CreateSubscription(context.Background(), "sub-name",
+		pubsub.SubscriptionConfig{Topic: topic})
+}
+
+func (l *AircraftList) Read() string {
+	l.RLock()
+	defer l.RUnlock()
+	return l.val
+}
+
+func (l *AircraftList) Write(newVal string) {
+	l.Lock()
+	defer l.Unlock()
+	l.val = newVal
+}
+
+var aircraftList AircraftList
+
+const startVal string = `
+{
+	"timestamp":0,
+	"list":[]
+}`
+
+func main() {
+
+	aircraftList.Write(startVal)
+
+	var credJson = mustGetenv(env_credJson)
+	var projectID = mustGetenv(env_projectID)
+	var topicID = mustGetenv(env_topicID)
+	var subID = mustGetenv(env_subID)
+
+	err := GcpInitialize(credJson, projectID)
+	checkErr(err)
+
+	go pubsubHandler(topicID, subID)
+
+	router := gin.New()
+
+	router.SetTrustedProxies(nil)
+	router.GET("/airspace/aircraft/position/:icao24", httpRequestHandler)
+
+	router.Run()
+}
+
+func httpRequestHandler(c *gin.Context) {
+	icao24 := c.Param("icao24")
+
+	c.String(http.StatusOK, aircraftList.Read())
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		panic("Environment variable not set: " + k)
+	}
+	return v
+}
+
+func pubsubHandler(topicID string, subscriptionID string) {
+
+	topic, err := GcpGetTopic(topicID)
+	checkErr(err)
+
+	sub, err := GcpGetSubscription(subscriptionID, topic)
+	checkErr(err)
+
+	err = sub.Receive(context.Background(), func(ctx context.Context, msg *pubsub.Message) {
+		messageHandler(subscriptionID, ctx, msg)
+	})
+	checkErr(err)
+}
+
+func messageHandler(subscriptionID string, ctx context.Context, msg *pubsub.Message) {
+	defer func() {
+		if r := recover(); r != nil {
+			msg.Ack()
+		}
+	}()
+
+	msg.Ack()
+
+	aircraftList.Write(string(msg.Data))
+}
