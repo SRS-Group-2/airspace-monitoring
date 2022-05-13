cdaf4c7d17f734076d8d7e07e637fb559ed61437
Gio-R <>
2022-05-13 21:22:02 +0200

    Comment out flink

--- terraform/kubernetes-flink-jobmanager-service.tf
+++ terraform/kubernetes-flink-jobmanager-service.tf
@@ -1,32 +1,32 @@
-resource "kubernetes_service" "flink_jobmanager" {
-  depends_on = [kubernetes_namespace.main_namespace]
-  metadata {
-    name      = "flink-jobmanager"
-    namespace = var.kube_namespace
-  }
+# resource "kubernetes_service" "flink_jobmanager" {
+#   depends_on = [kubernetes_namespace.main_namespace]
+#   metadata {
+#     name      = "flink-jobmanager"
+#     namespace = var.kube_namespace
+#   }
 
-  spec {
-    port {
-      name = "rpc"
-      port = 6123
-    }
+#   spec {
+#     port {
+#       name = "rpc"
+#       port = 6123
+#     }
 
-    port {
-      name = "blob-server"
-      port = 6124
-    }
+#     port {
+#       name = "blob-server"
+#       port = 6124
+#     }
 
-    port {
-      name = "webui"
-      port = 8081
-    }
+#     port {
+#       name = "webui"
+#       port = 8081
+#     }
 
-    selector = {
-      app = "flink"
+#     selector = {
+#       app = "flink"
 
-      component = "jobmanager"
-    }
+#       component = "jobmanager"
+#     }
 
-    type = "ClusterIP"
-  }
-}
+#     type = "ClusterIP"
+#   }
+# }
