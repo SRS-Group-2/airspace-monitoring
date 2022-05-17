9fa213324e90f49f11debc4d6c1c336a58ac506a
Gio-R <>
2022-05-17 11:18:26 +0200

    Add web_ui serving service

---
+++ terraform/cloud-run-web-ui.tf
@@ -0,0 +1,34 @@
+
+# Aircraft Info service
+resource "google_cloud_run_service" "web_ui" {
+  name     = "web-ui"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/docker-repo/web_ui:latest"
+      }
+    }
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "15"
+        "autoscaling.knative.dev/minScale" = "0"
+      }
+    }
+  }
+
+  # direct all traffic toward latest revision
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth_web_ui" {
+  location = google_cloud_run_service.web_ui.location
+  project  = google_cloud_run_service.web_ui.project
+  service  = google_cloud_run_service.web_ui.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
