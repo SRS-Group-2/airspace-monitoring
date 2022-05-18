b3b8fc899568ffe0b273839bb1f9fd2b304af690
Gio-R <>
2022-05-18 08:27:19 +0200

    Add positions service

---
+++ cloud-run-aircraft-positions.tf
@@ -0,0 +1,68 @@
+resource "google_service_account" "aircraft_positions_sa" {
+  account_id   = "aircraft-positions"
+  display_name = "A service account for the Aircraft Positions service"
+}
+
+# bind the service account to the necessary roles
+resource "google_project_iam_binding" "aircraft_positions_binding" {
+  project = var.project_id
+  role    = "roles/pubsub.editor"
+
+  members = [
+    "serviceAccount:${google_service_account.aircraft_positions_sa.email}",
+  ]
+}
+
+# Aircraft Daily History service
+resource "google_cloud_run_service" "aircraft_positions" {
+  depends_on = [
+    google_service_account.aircraft_positions_sa,
+    google_pubsub_topic.pubsub_positions
+  ]
+  name     = "aircraft-positions"
+  location = var.region
+
+  template {
+    spec {
+      service_account_name = google_service_account.aircraft_positions_sa.email
+      containers {
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/docker-repo/aircraft_positions:latest"
+        env {
+          name  = "GOOGLE_CLOUD_PROJECT_ID"
+          value = var.project_id
+        }
+
+        env {
+          name  = "GOOGLE_PUBSUB_AIRCRAFT_POSITIONS_TOPIC_ID"
+          value = var.vectors_topic
+        }
+
+        env {
+          name  = "GIN_MODE"
+          value = "release"
+        }
+      }
+    }
+
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "10"
+        "autoscaling.knative.dev/minScale" = "0"
+      }
+    }
+  }
+
+  # direct all traffic toward latest revision
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth_aircraft_positions" {
+  location = google_cloud_run_service.aircraft_positions.location
+  project  = google_cloud_run_service.aircraft_positions.project
+  service  = google_cloud_run_service.aircraft_positions.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
