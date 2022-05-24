0fe32021ac99acc9f6f72cd86b41dd97d099bb12
AL <26797547+Al12rs@users.noreply.github.com>
2022-05-24 16:54:58 +0200

    terraform: fix flink taskmanager image pull

--- terraform/kubernetes-flink-taskmanager-job-deployment.tf
+++ terraform/kubernetes-flink-taskmanager-job-deployment.tf
@@ -121,7 +121,7 @@ resource "kubernetes_deployment" "flink_taskmanager" {
         }
         container {
           name  = "taskmanager"
-          image = "${var.docker_repo_region}-docker.pkg.dev/${var.project_id}/${var.docker_repo_name}/states_source:latest"
+          image = "${var.docker_repo_region}-docker.pkg.dev/${var.project_id}/${var.docker_repo_name}/states_source:${var.states_source_tag}"
           args  = ["taskmanager"]
 
           env {
