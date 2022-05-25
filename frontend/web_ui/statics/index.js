278bd710a5bde477bbff136189944aff2b074f41
nicobari30 <71765091+nicobari30@users.noreply.github.com>
2022-05-25 18:44:03 +0200

    Update web UI and js

--- frontend/web_ui/statics/index.js
+++ frontend/web_ui/statics/index.js
@@ -67,20 +67,28 @@ function get_timestamp(date_selector, time_selector) {
 }
 
 function set_realtime_history(timeframe) {
+  
+  document.getElementById("current_error").innerText=""
+  
   request_value(base_history_route + "/realtime/" + timeframe, v => {
-    console.log(v)
-    var json = JSON.parse(v) // strange error here
-    const current_distance = document.getElementById("km")
-    const current_co2 = document.getElementById("co2")
-    document.getElementById("current_update").innerHTML =  get_date_string(json.startTime)
-    current_distance.innerText = json.distanceKm
-    current_co2.innerText = json.CO2t
+    if(v=="{}") {
+      document.getElementById("current_error").innerText = "No data available"
+      document.getElementById("km").innerText=""
+      document.getElementById("co2").innerText=""
+      document.getElementById("current_update").innerText=""
+    } else {
+      var json = JSON.parse(v) // strange error here
+      const current_distance = document.getElementById("km")
+      const current_co2 = document.getElementById("co2")
+      document.getElementById("current_update").innerHTML =  get_date_string(json.timestamp)
+      current_distance.innerText = json.distanceKm
+      current_co2.innerText = json.CO2t
+    }
   })
 }
 
 function show_daily_history(history) {
   var json = JSON.parse(history)
-
   table = document.getElementById("historic_distance")
   table.innerHTML=""
   table= document.createElement("table")
@@ -93,67 +101,61 @@ function show_daily_history(history) {
   var h_co2 = document.createElement("th")
   h_co2.innerHTML = "CO2 (t)"
   h_row.append(h_time, h_distance, h_co2)
-  table.append(h_row)
-  Object.entries(json).forEach(element => {
-    var row = document.createElement("tr")
-    var time = document.createElement("td")
-    time.innerHTML = get_onlydate_string(element[1]["startTime"])
-    var distance = document.createElement("td")
-    distance.innerHTML = element[1]["distanceKm"]
-    var co2 = document.createElement("td")
-    co2.innerHTML = element[1]["CO2t"]
-    row.append(time, distance, co2)
-    table.append(row)
-  })
-  // document.getElementById("historic_distance").innerHTML = ""
+  var rowDel = document.createElement("tr")
+  rowDel.setAttribute("id","toDelete")
+  for(i=0;i<=2;i++) {
+    var toAdd=document.createElement("td")
+    rowDel.append(toAdd)
+  }
+  table.append(h_row,rowDel)
   document.getElementById("historic_distance").append(table)
+  if(Object.entries(json.history).length===0) {
+    const error_paragraph = document.createElement("p")
+    error_paragraph.id = "form_error"
+    error_paragraph.innerHTML = "No data available in this interval"
+    error_paragraph.setAttribute("class","text")
+    document.getElementById("historic_data_form").append(error_paragraph)
+  } else {
+    Object.entries(json.history).forEach(element => {
+      document.getElementById("toDelete").innerHTML=""
+      var time = document.createElement("td")
+      var row = document.createElement("tr")
+      if(json.resolution=="day") {
+        time.innerHTML= get_onlydate_string(element[1]["startTime"])
+      }
+      else if (json.resolution=="hour") {
+        time.innerHTML = get_date_string(element[1]["startTime"])
+      }
+      var distance = document.createElement("td")
+      distance.innerHTML = element[1]["distanceKm"]
+      var co2 = document.createElement("td")
+      co2.innerHTML = element[1]["CO2t"]
+      row.append(time, distance, co2)
+      table.append(row)
+    })
+    document.getElementById("historic_distance").append(table)
+  }
 }
 
 
-function show_hourly_history(history) {
-  var json = JSON.parse(history)
-
-  table = document.getElementById("historic_distance")
-  table.innerHTML=""
-  table= document.createElement("table")
-  table.setAttribute('id','historical-data')
-  var h_row = document.createElement("tr")
-  var h_time = document.createElement("th")
-  h_time.innerHTML = "Time"
-  var h_distance = document.createElement("th")
-  h_distance.innerHTML = "Distance (km)"
-  var h_co2 = document.createElement("th")
-  h_co2.innerHTML = "CO2 (t)"
-  h_row.append(h_time, h_distance, h_co2)
-  table.append(h_row)
-  Object.entries(json).forEach(element => {
-    var row = document.createElement("tr")
-    var time = document.createElement("td")
-    time.innerHTML = get_date_string(element[1]["startTime"])
-    var distance = document.createElement("td")
-    distance.innerHTML = element[1]["distanceKm"]
-    var co2 = document.createElement("td")
-    co2.innerHTML = element[1]["CO2t"]
-    row.append(time, distance, co2)
-    table.append(row)
-  })
-  // document.getElementById("historic_distance").innerHTML = ""
-  document.getElementById("historic_distance").append(table)
-}
 
 function get_date_string(timestamp) {
-  console.log(timestamp)
   var args = timestamp.split("-").map(s => parseInt(s))
-  return (new Date(...args)).toLocaleString()
+  args[1]=args[1]-1
+  return (new Date(Date.UTC(...args))).toLocaleString()
 }
 
 function get_onlydate_string(timestamp) {
   var args = timestamp.split("-").map(s => parseInt(s))
-  return (new Date(...args)).toLocaleDateString()
+  args[1]=args[1]-1
+  return (new Date(Date.UTC(...args))).toLocaleDateString()
 }
 
 function set_selectable_italian_flights() {
   const flight_selector = document.getElementById("flight")
+  if(flight_selector.value.length==6){
+    flight=flight_selector.value
+  }
   request_value(base_aircraft_route + "/list", vs => {
     var json = JSON.parse(vs)
     document.getElementById("flights_list_update").innerHTML = "Aircraft list updated at " + get_date_string(json.timestamp)
@@ -164,10 +166,14 @@ function set_selectable_italian_flights() {
     flight_selector.append(emptyOption)
     json["icao24"].map(v => {
       var opt = document.createElement("option")
+      opt.setAttribute("id",v)
       opt.value = v
       opt.innerHTML = v
       flight_selector.append(opt)
     })
+    if(flight.length==6) {
+      document.getElementById(flight).setAttribute("selected",true)
+    }
   })
 }
 
@@ -198,46 +204,39 @@ function update_aircraft_position(data) {
 }
 
 window.onload = _ => {
+  document.getElementById("loading").innerText=""
   var websocket = null
   /* set up flight selector */
   const flight_selector = document.getElementById("flight")
-  set_selectable_italian_flights()
   flight_selector.onclick = _ev => {
     set_selectable_italian_flights()
   }
   flight_selector.onchange = _ev => {
+    document.getElementById("startedListening").innerText="Started listening positions at "+ new Date(Date.now()).toLocaleString()
     document.getElementById("notFound").innerText=""
     document.getElementById("icao24").innerText=""
     document.getElementById("latitude").innerText=""
     document.getElementById("longitude").innerText=""
     document.getElementById("updatetime").innerText=""
-    
-    if (flight_selector.value === " ") {
-      document.getElementById("icao").innerText=""
-      document.getElementById("manufacturer").innerText=""
-      document.getElementById("model").innerText=""
-      document.getElementById("registration-n").innerText=""
-      document.getElementById("serial-number").innerText=""
-      if (websocket !== null) {
+    if (websocket !== null) {
         websocket.close()
-      }
-    } else {
-      flight=flight_selector.value
-      request_value(base_aircraft_route + "/" + flight + "/info", v => {
-        set_aircraft_info(v)})
-      request_value("/endpoints/position/url",v => {
-        var url = v.replace("https://", "wss://") + base_aircraft_route + "/" + flight + "/position"
-        websocket = new WebSocket(url)
-       websocket.addEventListener('message', ev =>  {
-        update_aircraft_position(ev.data)
-          })
-        websocket.onclose = function (event) {
-          if(event.code == 1006){
-            alert("The connection was closed abnormally: code 1006")
-          }
-        }
-      })
     }
+    flight=flight_selector.value
+    request_value(base_aircraft_route + "/" + flight + "/info", v => {
+      set_aircraft_info(v)})
+      request_value("/endpoints/position/url", v => {
+      var url = v.replace("https://", "wss://") + base_aircraft_route + "/" + flight + "/position"
+      websocket = new WebSocket(base_aircraft_route + "/" + flight + "/position")
+      websocket.onopen = function(event) {
+        document.getElementById("loading").innerText="Waiting for next position..."
+      }
+      websocket.addEventListener('message', ev =>  {
+          update_aircraft_position(ev.data)
+      })
+      websocket.onclose = function (event) {
+          document.getElementById("loading").innerText="No more position available"
+      }
+    })
   }
   /* set up current distance, co2 */
   const current_timeframe_selector = document.getElementById("current_timeframe")
@@ -285,16 +284,12 @@ window.onload = _ => {
         "to": get_end_timestamp(),
         "resolution": historic_resolution_selector.value
       }
-      if (historic_resolution_selector.value === "hour") {
-        request_value_with_params(base_history_route, show_hourly_history, params)
-      }
-      else if (historic_resolution_selector.value === "day") {
-        request_value_with_params(base_history_route, show_daily_history, params)
-      }
+      request_value_with_params(base_history_route, show_daily_history, params)
     } else {
       const error_paragraph = document.createElement("p")
       error_paragraph.id = "form_error"
       error_paragraph.innerHTML = "Missing fields"
+      error_paragraph.setAttribute("class","text")
       document.getElementById("historic_data_form").append(error_paragraph)
     }
   }
