15446db23781df840ad6db0b2974e697169dd9ea
AL <26797547+Al12rs@users.noreply.github.com>
2022-05-27 18:48:06 +0200

    Implemented some scenarios in gatling: - aircraft list - list+ info - realtime history

--- gatling_simulations/aircraft/RecentHistorySimulation.scala
+++ gatling_simulations/aircraft/RecentHistorySimulation.scala
@@ -2,11 +2,13 @@ package aircraft
 
 import scala.concurrent.duration._
 import scala.util.Random
+import java.util.Locale
+
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class ListSimulation extends Simulation {
+class RecentHistorySimulation extends Simulation {
   val BASE_URL = System.getProperty("base_url")
   val N_USERS = Integer.getInteger("users", 20)
   val RAMP = java.lang.Long.getLong("ramp", 0)
@@ -20,16 +22,22 @@ class ListSimulation extends Simulation {
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
   val rand = scala.util.Random
-  val scn = scenario("ListScenario")
+  val resolutionValues = Seq("1h", "6h", "24h")
+
+  val scn = scenario("RecentHistoryScenario")
+    .exec{ session =>
+        session.set("resolution", resolutionValues(rand.nextInt(resolutionValues.length)))
+    }
     .exec(
-      http("Aircraft list request")
-        .get("/airspace/aircraft/list")
+      http("Airspace recent history request")
+        .get(session => ("/airspace/history/realtime/" + session("resolution").as[String]).toLowerCase(java.util.Locale.getDefault))
     )
 
   setUp(
     scn.inject(
       atOnceUsers(10),
-      rampUsers(100).during(10),
-      constantUsersPerSec(400).during(15))
+      rampUsers(50).during(10),
+      rampUsers(50).during(10),
+      constantUsersPerSec(100).during(10))
   ).protocols(httpProtocol)
 }
