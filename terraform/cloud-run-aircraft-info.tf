a009e0d0ed8e2fdc5abd8b77e84a82e138824c74
Gio-R <>
2022-05-27 12:10:40 +0200

    Prepare for manual setup of service accounts

--- terraform/cloud-run-aircraft-info.tf
+++ terraform/cloud-run-aircraft-info.tf
@@ -12,6 +12,13 @@ resource "google_project_iam_binding" "aircraft_info_binding_log" {
   ]
 }
 
+locals {
+  aircraft_info_sa_name = google_service_account.aircraft_info_sa.name
+  aircraft_info_sa_email = google_service_account.aircraft_info_sa.email
+  # aircraft_info_sa_email = "aircraft-info@${var.project_id}.iam.gserviceaccount.com"
+  # aircraft_info_sa_name  = "projects/${var.project_id}/serviceAccounts/${local.aircraft_info_sa_email}"
+}
+
 # Aircraft Info service
 resource "google_cloud_run_service" "aircraft_info" {
   depends_on = [
@@ -25,6 +32,7 @@ resource "google_cloud_run_service" "aircraft_info" {
 
   template {
     spec {
+      service_account_name = local.aircraft_info_sa_email
       container_concurrency = 1000
       containers {
         image = "${var.docker_repo_region}-docker.pkg.dev/${var.project_id}/${var.docker_repo_name}/aircraft_info:${var.aircraft_info_tag}"
--- terraform/cloud-run-aircraft-positions.tf
+++ terraform/cloud-run-aircraft-positions.tf
@@ -22,6 +22,13 @@ resource "google_project_iam_binding" "aircraft_positions_binding_log" {
   ]
 }
 
+locals {
+  aircraft_positions_sa_name = google_service_account.aircraft_positions_sa.name
+  aircraft_positions_sa_email = google_service_account.aircraft_positions_sa.email
+  # aircraft_positions_sa_email = "aircraft-positions@${var.project_id}.iam.gserviceaccount.com"
+  # aircraft_positions_sa_name  = "projects/${var.project_id}/serviceAccounts/${local.aircraft_positions_sa_email}"
+}
+
 # Aircraft Daily History service
 resource "google_cloud_run_service" "aircraft_positions" {
   depends_on = [
@@ -33,7 +40,7 @@ resource "google_cloud_run_service" "aircraft_positions" {
 
   template {
     spec {
-      service_account_name  = google_service_account.aircraft_positions_sa.email
+      service_account_name  = local.aircraft_positions_sa_email
       container_concurrency = 1000
       containers {
         image = "${var.docker_repo_region}-docker.pkg.dev/${var.project_id}/${var.docker_repo_name}/aircraft_positions:${var.aircraft_positions_tag}"
